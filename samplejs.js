d0fdb14733fdebc75e35fd160512d0762dcf6997
topashgun <topashgun@gmail.com>
2024-11-25 15:49:16 +0530

    Update samplejs.js

--- samplejs.js
+++ samplejs.js
@@ -1,1 +1,19 @@
 console.log("Page has successfully loaded!");
+
+
+function getRandomData() {
+    const randomNumber = Math.floor(Math.random() * 100); // Random number between 0-99
+    const randomTexts = [
+        "Hello, world!",
+        "JavaScript is awesome!",
+        "Have a great day!",
+        "Coding is fun!",
+        "Keep learning, keep growing!"
+    ];
+    const randomText = randomTexts[Math.floor(Math.random() * randomTexts.length)]; // Random text
+
+    return {
+        number: randomNumber,
+        text: randomText
+    };
+}
